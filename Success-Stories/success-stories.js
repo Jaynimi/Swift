f48a7c92d41eeea9fd94c820de539d883ebd71dd
Jaynimi <Eyeslikejays@yahoo.com>
2023-05-25 02:28:38 +0200

    footer and success text

---
+++ Success-Stories/success-stories.js
@@ -0,0 +1,43 @@
+// Nav Toggle
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container")
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    // Found the actual lenght of the ul with class of links instead of manually calculating and inputting it
+    // links.classList.toggle("show-links");
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    if(containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight + 50}px`
+    } else {
+        linksContainer.style.height = 0;
+    }
+
+    console.log()
+    
+});
+
+// Fixed Navbar on scroll and Back-to-top button
+const navbar = document.getElementById("nav");
+const backToTop = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    // fixed navbar
+    if(scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+
+    // back-to-top button
+    if (scrollHeight > 450) {
+        backToTop.classList.add("show-link");
+    } else {
+        backToTop.classList.remove("show-link")
+    }
+});
